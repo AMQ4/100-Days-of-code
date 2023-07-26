adcc9bdf50284efb5050e6dcdb8a58944656b981
AMQ4 <amq4319@gmail.com>
2023-07-27 01:15:53 +0300

    * all dic. comp. exercises added

---
+++ day 26/exercise/Dictionary Comprehension.py
@@ -0,0 +1,6 @@
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# Don't change code above 👆
+result = {string: len(string) for string in sentence.split()}
+# Write your code below:
+
+print(result)
