ba1dc43f9a9c9f0af5c8095bbc2b8dc1e03b6247
AMQ4 <amq4319@gmail.com>
2023-07-30 20:58:45 +0300

    * day 26 `main.py`, `try` block added to catch and   handle the exceptions

--- day 26/final project/main.py
+++ day 26/final project/main.py
@@ -8,16 +8,24 @@ file = pandas.read_csv("./nato_phonetic_alphabet.csv")
 # The keys are the 'letter' column values, and the values are the 'code' column values from the DataFrame.
 d = {value.letter: value.code for (key, value) in file.iterrows()}
 
-# Ask the user to enter a word and convert it to uppercase to ensure consistency.
-word = input("Enter a word: ").upper()
 
 # Create a list 'word_in_nato' to store the NATO phonetic alphabet codes for each letter in the input word. The list
-# is generated using a list comprehension, where each letter of the input word is looked up in the 'd' dictionary.
-word_in_nato = [d[letter] for letter in word]
+# is generated using a list comprehension, where each letter of the input word is looked up in the 'd' dictionary, else,
+# a KeyError exception will be thrown.
+def print_word_in_nato():
+    # Ask the user to enter a word and convert it to uppercase to ensure consistency.
+    word = input("Enter a word: ").upper()
+    try:
+        word_in_nato = [d[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters in alphabet please.")
+        print_word_in_nato()
+    # Print the list of NATO phonetic alphabet codes corresponding to the letters in the input word.
+    else:
+        print(word_in_nato)
 
-# Print the list of NATO phonetic alphabet codes corresponding to the letters in the input word.
-print(word_in_nato)
 
+print_word_in_nato()
 # However, we can do better using a few of line of code :
 
 # Create a list 'word_in_nato' to store the NATO phonetic alphabet codes for each letter in the input word. The list
